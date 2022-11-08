21608b7e8a541b579dc979dada7ef8238c1cce5e
Shafin Chowdhury <101200161+Shafin-chowdhury@users.noreply.github.com>
2022-11-08 19:50:10 +0600

    Add files via upload

---
+++ for loop(pattern)/index.js
@@ -0,0 +1,226 @@
+//1.
+for(let i=1; i<=5; i++){
+    // for(let k=1; k<=5-i; k++){
+    //     document.write(" ")
+    // }
+
+    for(let j=1; j<=i; j++){
+        document.write("*");
+    }
+    document.write("</br>");
+
+}
+
+//2.
+
+for (let i=1; i<=5; i++){
+    for(let k = 1; k<=5-i; k++){
+        document.write("&nbsp;&nbsp")
+    }
+
+    for(let j=1; j<=i; j++){
+        document.write("*")
+    }
+    document.write("</br>")
+}
+
+// 3.
+
+for (let i=1; i<=5; i++){
+    for(let k = 1; k<=5-i; k++){
+        document.write("&nbsp;&nbsp")
+    }
+
+    for(let j=1; j<=i; j++){
+        document.write("*")
+    }
+for(let l=2; l<=i; l++){
+    document.write("*")
+}
+
+    document.write("</br>")
+}
+//4.
+
+for(let i = 1; i<=9; i++){
+
+    for(let k = 1; k<=5; k++){
+                document.write(" ")
+             }
+    for(let j = 1; j<=i; j++){
+       document.write("*")
+    }
+    document.write("</br>")
+}
+
+//5.
+
+for (var i=1; i<=5; i++){
+
+   for(var j=1; j<=i; j++){
+document.write(i)
+   }
+   document.write("</br>")
+}
+
+//6.
+for (var i=1; i<=5; i++){
+
+   for(var j=1; j<=i; j++){
+document.write(i)
+   }
+   document.write("</br>")
+}
+
+//7.
+for (var i=1; i<=5; i++){
+
+   for(var j=1; j<=i; j++){
+document.write(j)
+   }
+   document.write("</br>")
+}
+
+
+//8.
+// for (let i=1; i<=5; i++){
+//    for(let k = 1; k<=5-i; k++){
+//        document.write("&nbsp;&nbsp")
+//    }
+
+//    for(let j=1; j<=2 *i -1; j++){
+//        document.write(j)
+//    }
+// // for(let l=2; l<=i; l++){
+// //    document.write(i)
+// // }
+
+//    document.write("</br>")
+// }
+
+//8.
+
+
+
+
+
+// for(let i=1; i<=5; i++){
+//     for(let k = 1; k<=5-i; k++){
+//                 document.write("&nbsp;&nbsp; &nbsp; &nbsp;")
+//             }
+//     for(let j=1; j=4-i; j++){
+
+//         if(i===1){
+//             if(j===1){
+//                 //document.write("&nbsp; &nbsp;")
+//                 document.write("*")  
+//             }
+//             } 
+//             //document.write("*")   
+       
+//     }
+//  document.write("</br>")
+
+// }
+
+
+
+// for(let i=1; i<=5; i++){
+//     for(let j=1; j<=5; j++){
+        
+//         if(i===1){
+//             if(j===1){
+//                 document.write("*")
+//             }
+//        else   if(i===2){
+//       if (j===1)
+//         {
+//            document.write("0")
+//         }
+//          else (j===2)
+//         {
+//            document.write("0")
+//         }
+//          }
+//         }
+//      }
+
+    
+//         document.write(" ")
+//     }
+
+// for(let i=1; i<=5; i++){
+//     for(let j=1; j<=9; j++){
+//         if(i===1){
+//             if(j===1){
+//                 document.write("*")
+//             }
+//         }
+//     }
+//     document.write("</br>")
+// }
+
+// for(let i=1; i<=9; i++){
+//     for(let j=1; j<=9; j++){
+//         if(i===1){
+
+//             if(j===1){
+// document.write("&nbsp;")
+//             }
+//             else if(j===2){
+//                 document.write("&nbsp;")
+//             }
+//             else if(j===3){
+//                 document.write("&nbsp;")
+//             }
+//             else if  (j===4){
+//                 document.write("&nbsp;")
+//             }
+
+//             else if  (j===5){
+//                 document.write("9")
+//             }
+//         }
+
+//         if(i===2){
+
+//             if(j===1){
+// document.write(".")
+//             }
+//             else if(j===2){
+//                 document.write("/")
+//             }
+//             else if(j===3){
+//                 document.write("\\")
+//             }
+//             else if (j===4){
+//                 document.write(".")
+//             }
+//         }
+//     }
+//     document.write("</br>")
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
